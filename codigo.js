608ab8937854ab92ade44f378ef7b1462a515c09
ThunderGer <jaames11@hotmail.com>
2021-05-10 00:55:38 -0500

    update files

---
+++ codigo.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const horaContainer = document.querySelector('.hora');
+const minutosContainer = document.querySelector('.minutos');
+const segundosContainer = document.querySelector('.segundos');
