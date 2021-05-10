4c6b0e912506fdfdf85f1281d11db03fdecde65f
ThunderGer <jaames11@hotmail.com>
2021-05-10 03:17:02 -0500

    end app one feature

--- codigo.js
+++ codigo.js
@@ -1,5 +1,15 @@
 'use strict'
 
 const horaContainer = document.querySelector('.hora');
-const minutosContainer = document.querySelector('.minutos');
-const segundosContainer = document.querySelector('.segundos');
+const fechaContainer = document.querySelector('.fecha');
+const mensajeContainer = document.querySelector('.text');
+const updateHora = setInterval(function() {
+    const date = new Date();
+    let time = (date.getHours >= 12) ? "PM" : "AM"
+    horaContainer.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${time}`
+    fechaContainer.innerHTML = `${date.toDateString()}`
+
+    if (date.toDateString()[8] === "2" && date.toDateString()[9] === "0") {
+        mensajeContainer.innerHTML = "Hoy es tu aniversario"
+    }
+}, 1000);
